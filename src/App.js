f38e2a6ba67ea6f67abcd3c148fce17626d20991
Max Roustikov <roustikov@gmail.com>
2020-11-26 18:50:28 +0300

    feat: Implemented the basic Tab bar, split Views into Tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import TaskForm from './TaskForm.js'
+import ReactGantt from 'gantt-for-react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+var React = require('react');
 
-export default App;
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewMode: "Month",
+            tasks: [ 
+                { id:"Task1", name:"Test1", start:"2020-11-20", end:"2021-02-24", progress:"80", dependencies:"" },
+                { id:"Task2", name:"Test2", start:"2020-11-21", end:"2021-04-24", progress:"10", dependencies:"" },
+                { id:"Task3", name:"Test3", start:"2020-11-23", end:"2021-04-26", progress:"30", dependencies:"Task1,Task2" },
+                { id:"Task4", name:"Test4", start:"2020-11-23", end:"2021-06-26", progress:"10", dependencies:"Task3" },
+            ]
+        }
+    }
+    
+    addTask = (taskData) => {
+        this.state.tasks.push(taskData);
+        this.setState(this.state);
+    }
+    
+    getTasks(){
+        return this.state.tasks;
+    }
+    
+    render() {
+        return (
+            <div className="App">
+            <Tabs>
+            <TabList>
+            <Tab>Edit Tasks</Tab>
+            <Tab>Kanban Board</Tab>
+            <Tab>Mind Map</Tab>
+            <Tab>Gantt Diagram</Tab>
+            </TabList>
+            
+            <TabPanel>
+            <TaskForm addTaskCallback={this.addTask}></TaskForm>            
+            </TabPanel>
+
+            <TabPanel>
+            </TabPanel>
+
+            <TabPanel>
+            </TabPanel>
+
+            <TabPanel>
+                <div>
+                    <ReactGantt tasks={this.getTasks()} viewMode={this.state.viewMode}></ReactGantt>
+                </div>
+            </TabPanel>
+
+            </Tabs>
+            
+            </div>);
+        }
+    }
+    
+    export default App;
+    
---
+++ src/TaskForm.js
@@ -0,0 +1,112 @@
+var React = require('react');
+
+class TaskForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        id: "Task6",
+        name: "Task6",
+        description: "Test",
+        assignee: "User",
+        progress: "80",
+        start: "2020-11-23",
+        end: "2021-11-23"
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  addTask = (event) => {
+    this.props.addTaskCallback(this.state);
+    // generate new id
+    this.setState({
+        id: "",
+        name: "",
+        description: "",
+        assignee: "",
+        progress: "",
+        start: "",
+        end: ""
+    })
+    
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit = {this.addTask}>
+        <div className="container">
+      <label>Create new task</label>
+
+        <div className="input-group">
+        <label>Title:</label>
+        <input
+          name="name"
+          type="input"
+          value={this.state.name}
+          onChange={this.handleInputChange} />
+          </div>
+
+        <div className="input-group">
+        <label>Description:</label>
+          <input
+            name="description"
+            type="text"
+            value={this.state.description}
+            onChange={this.handleInputChange} />
+        </div>
+
+        <div className="input-group">
+        <label>Assignee:</label>
+          <input
+            name="assignee"
+            type="text"
+            value={this.state.assignee}
+            onChange={this.handleInputChange} />
+        </div>
+
+        <div className="input-group">
+        <label>Progress:</label>
+          <input
+            name="progress"
+            type="text"
+            value={this.state.progress}
+            onChange={this.handleInputChange} />
+        </div>
+        
+        <div className="input-group">
+        <label>Start date:</label>
+          <input
+            name="start"
+            type="text"
+            value={this.state.start}
+            onChange={this.handleInputChange} />
+        </div>
+        
+        <div className="input-group">
+        <label>End date:</label>
+          <input
+            name="end"
+            type="text"
+            value={this.state.end}
+            onChange={this.handleInputChange} />
+        </div>
+        <input className="add-task-button" type="submit" value = "Add task"/>
+        </div>
+      </form>
+      
+    );
+  }
+}
+
+export default TaskForm;
