bb9a7939d6039b3dbea0b61a90377a4059135e1f
Max Roustikov <roustikov@gmail.com>
2020-12-11 20:43:59 +0300

    feat: added kanban board

---
+++ src/Kanban/KanbanBoard.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-react-kanban";
+import './kanban.css';
+import { extend, addClass } from '@syncfusion/ej2-base';
+import * as dataSource from './datasource.json';
+
+
+export default class KanbanBoard extends React.Component {
+    props:any;
+    fields:any;
+    constructor(props:any) {
+        super(props);
+    }
+    render() {
+        return (<div className='schedule-control-section'>
+        <div className='col-lg-12 control-section'>
+          <div className='control-wrapper'>
+            <KanbanComponent id="kanban" 
+            cssClass="kanban-overview" 
+            keyField="State" 
+            dataSource={this.props.tasks}
+            cardSettings={{ showHeader:false, headerField: "id", contentField: "TaskName"}} 
+            dialogSettings={{ fields: this.fields }}>
+              <ColumnsDirective>
+                <ColumnDirective headerText="To Do" keyField="ToDo"/>
+                <ColumnDirective headerText="In Progress" keyField="InProgress"/>
+                <ColumnDirective headerText="In Review" keyField="Review"/>
+                <ColumnDirective headerText="Done" keyField="Done"/>
+              </ColumnsDirective>
+            </KanbanComponent>
+          </div>
+        </div>
+      </div>);
+    }
+}
